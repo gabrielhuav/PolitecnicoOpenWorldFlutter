d5bfae6bfaaa954970fbf0f09bc5e0b546127392
SamuelBC25 <irvinbuendia053@gmail.com>
2026-04-29 04:16:33 -0600

    migrar entidades core y dependencias estables
    Instalación del stack definitivo (flutter_map, riverpod, drift, geolocator). Migración estricta de MapNode, MapWay, Npc, CarModel y NpcType a Dart puro implementando equatable y uuid.

--- politecnicoopenworldflutter/test/widget_test.dart
+++ politecnicoopenworldflutter/test/widget_test.dart
@@ -8,8 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'lib/main.dart';
-
+import 'package:politecnicoopenworldflutter/main.dart';
+  
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
